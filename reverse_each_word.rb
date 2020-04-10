0859d1d61f48799d36a27b7b2e44d2c04cabea9b
Phyllis N <phyllisgetcity@gmail.com>
2020-04-10 19:44:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,36 +5,15 @@ Remember that .each returns the original array but other enumerators don't.
 =end
 
 def reverse_each_word(string)
-=begin
-p string
-p string.class
-phrase = string
-p phrase
-=end
-=begin
-  array_of_words = {
-    name: %w(string)
-  }
-=end
-#array_of_words = []
-array_of_words = string.split(" " )
-#somehow, doesnt split. only assigns the whole string to first element of array
-  #p %w(string)
-=begin
-    p array_of_words
-  p array_of_words[0]
-  p array_of_words[1]
-=end
-    new_array = []
-      array_of_words.each do |element|
-      #  p element.class
-        new_array << element.reverse
-        end
-    #  p array_of_words
-    #  p new_array
-    #new_array
-    #{}"'#{['12','34','35','231'].join("', '")}'"
-    p new_array.join(" ")
+
+  array_of_words = string.split(" " )
+
+  new_array = []
+  array_of_words.each do |element|
+    new_array << element.reverse
+    end
+  
+  p new_array.join(" ")
 
     array_of_words.collect do |element|
       element.reverse
