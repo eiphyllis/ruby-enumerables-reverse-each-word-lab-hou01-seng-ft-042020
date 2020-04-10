9fdad593e3cac7bf49647f3abb5e239caf7e37ca
Phyllis N <phyllisgetcity@gmail.com>
2020-04-10 19:51:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -17,7 +17,7 @@ def reverse_each_word(string)
   p (array_of_words.each do |element|
     element.reverse
   end).join(" ")
-  
+
   (array_of_words.collect do |element|
     element.reverse
   end).join(" ")
