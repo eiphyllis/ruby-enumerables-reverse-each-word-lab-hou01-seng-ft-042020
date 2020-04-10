21c572afce17271e6e825ba6d902ad20d0834c1d
Phyllis N <phyllisgetcity@gmail.com>
2020-04-10 19:45:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,7 +16,7 @@ def reverse_each_word(string)
   p new_array.join(" ")
 
   array_of_words.collect do |element|
-    p element.reverse
+    element.reverse
     end
     #i got the elements to reverse but idk how to put them into single string
     #bc array_of_words.join doesnt have the elements reversed anymore
